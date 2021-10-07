a35651903632bf68f01511dcf059abc4f88b3642
Aleksandr <dentranscom@gmail.com>
2021-10-07 21:54:47 +0300

    POST

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,4 +18,45 @@ const counters = document.querySelectorAll('.skills__progress-counter'),
     });
 
 
+               //Post
 
+	function valideForms(form){
+		$(form).validate({
+			rules: {
+				name: "required",
+				email: {
+					required: true,
+					email: true
+				}
+			},
+			messages: {
+				name: "Пожалуйста, введите свое имя",
+				email: {
+				  required: "Пожалуйста, введите свою почту",
+				  email: "Неправильно введен адрес почты"
+				}
+			}
+		});
+	};
+
+    valideForms('#contacts-form');
+	valideForms('#contacts form');
+	valideForms('#order form');
+
+	//$('input[name=phone]').mask("+38 (999) 999-99-99");
+
+	$('form').submit(function(e) {
+		e.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			$('#contacts, #order').fadeOut();
+			$('.overlay, #thanks').fadeIn('slow');
+
+			$('form').trigger('reset');
+		});
+		return false;
+	});
