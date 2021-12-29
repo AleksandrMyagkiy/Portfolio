dbbb5fd75daa2a312e7a9489631b809f6f2d71c4
Aleksandr <dentranscom@gmail.com>
2021-12-29 17:45:30 +0200

    jshintrc

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,62 +1,19 @@
 const hamburger = document.querySelector('.hamburger'),
-    menu = document.querySelector('.menu'),
-    closeElem = document.querySelector('.menu__close');
+	menu = document.querySelector('.menu'),
+	closeElem = document.querySelector('.menu__close');
 
 hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
+	menu.classList.add('active');
 });
 
 closeElem.addEventListener('click', () => {
-    menu.classList.remove('active');
+	menu.classList.remove('active');
 });
 
 const counters = document.querySelectorAll('.skills__progress-counter'),
-    lines = document.querySelectorAll('.skills__progress-line span');
+	lines = document.querySelectorAll('.skills__progress-line span');
 
-    counters.forEach( (item, i) => {
-        lines[i].style.width = item.innerHTML;
-    });
+counters.forEach((item, i) => {
+	lines[i].style.width = item.innerHTML;
+});
 
-
-               //Post
-
-	function valideForms(form){
-		$(form).validate({
-			rules: {
-				name: "required",
-				email: {
-					required: true,
-					email: true
-				}
-			},
-			messages: {
-				name: "Пожалуйста, введите свое имя",
-				email: {
-				  required: "Пожалуйста, введите свою почту",
-				  email: "Неправильно введен адрес почты"
-				}
-			}
-		});
-	};
-
-    valideForms('#contacts-form');
-	valideForms('#contacts form');
-	valideForms('#order form');
-
-	//$('input[name=phone]').mask("+38 (999) 999-99-99");
-
-	$('form').submit(function(e) {
-		e.preventDefault();
-		$.ajax({
-			type: "POST",
-			url: "mailer/smart.php",
-			data: $(this).serialize()
-		}).done(function() {
-			$(this).find("input").val("");
-			$('#contacts, #order').fadeOut();
-			$('.overlay, #thanks').fadeIn('slow');
-
-			$('form').trigger('reset');
-		});
-		return false;
-	});
